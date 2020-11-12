3f21812c93e5c29e67bb311794f06cabe833fea0
Tom Chesters <tom@tomaaato.xyz>
2020-11-12 22:30:48 +0000

    CMS UI

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,32 @@
+import "regenerator-runtime/runtime"
 import React from 'react'
 import Loading from './components/loading'
 import DefaultPage from './pages/default'
-import TextBlock from './blocks/text'
-import runApp from 'blux-app'
 import NotFound from './components/notFound'
+import TextBlock from './blocks/text'
 
-const demoApp = {
-    components: {
-        header: () => <h1>Hello World</h1>,
-        footer: () => null,
-        loading: Loading,
-        notFound: NotFound,
-    },
-    pages: [
-        DefaultPage
-    ],
-    blocks: [
-        TextBlock
-    ],
-    cms: () => import('./cms'),
-    isDev: true
+const runApp = async () => {
+    const demoApp = {
+        components: {
+            header: () => <h1>Hello World</h1>,
+            footer: () => null,
+            loading: Loading,
+            notFound: NotFound,
+        },
+        pages: [
+            DefaultPage
+        ],
+        blocks: [
+            TextBlock
+        ],
+        cms: () => import('./cms'),
+        isDev: true
+    }
+
+    window.React = React
+    const runBluxApp = (await import('blux-app')).default
+    
+    runBluxApp(demoApp, process.env.IS_CMS)
 }
 
-runApp(demoApp, process.env.IS_CMS)
+runApp()
--- src/cms/components/CmsUi/index.jsx
+++ src/cms/components/CmsUi/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const CmsUi = ({}) => {
-    return null
+const CmsUi = ({prop1}) => {
+    // Effects
+    // - Send update request to Page REST API
+    React.useEffect(() => {
+        // Send Request
+        
+    })
+    // Render
+    return <p>Hello World</p>
 }
 
 export default CmsUi
